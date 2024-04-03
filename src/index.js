a91eeecd23da86f11ce06dcbe847197c4e8287ed
khanhphan2609 <103134275+khanhphan2609@users.noreply.github.com>
2024-04-03 15:36:01 +0700

    refactor ocde: creates, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -17,13 +25,10 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Home, search, contact
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on localhost:${port}`);
